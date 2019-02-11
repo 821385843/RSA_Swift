c3da61202ee2ea8e467419f275e7678a99c77041
xiewei <821385843@qq.com>
2019-02-11 18:42:26 +0800

    co

--- Example/RSA_Swift/ViewController.swift
+++ Example/RSA_Swift/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        Thread.sleep(forTimeInterval: 3)
         
         rsaEncryptOrDecryptForString()
         md5String()
